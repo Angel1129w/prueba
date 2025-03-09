4fd2bb23581b52ceb13501cadfe8e87c59024352
Angel1129w <166573620+Angel1129w@users.noreply.github.com>
2025-03-09 15:48:39 -0500

    Delete LA GRANJA/test.py

--- LA GRANJA/test.py
+++
@@ -1,8 +0,0 @@
-import oracledb
-
-dsn = "localhost:1521/XEPDB1"
-try:
-    conexion = oracledb.connect(user="C##ANGEL", password="M423i32R4", dsn=dsn)
-    print("✅ Conexión exitosa a Oracle")
-except Exception as e:
-    print(f"❌ Error de conexión: {e}")
